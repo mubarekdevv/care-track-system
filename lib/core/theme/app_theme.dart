617b818c852dbe789cd5c20d3624153fa90e5e56
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-29 13:29:11 +0300

    dark themes implemented

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  // 1. Define Primary Colors
+  // 1. Light Mode colors
   static const Color primaryBlue = Color(0xFF4A90E2);
   static const Color softGreen = Color(0xFF7ED321);
   static const Color warmNeutral = Color(0xFFF5F7FA);
   static const Color textDark = Color(0xFF2D3436);
 
+  //1. Dark Mode colors
+
+  static const Color darkBackground  = Color(0xFF121212); // Deep Black/Grey
+  static const Color darkSurface = Color(0xFF1E1E1E); // Card color
+  static const Color primaryBlueDark = Color(0xFF64B5F6); // Lighter blue for dark mode
+
+  //Light Theme
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
+      brightness: Brightness.light,
       colorScheme: ColorScheme.fromSeed(
+        brightness: Brightness.light,
         seedColor: primaryBlue,
         primary: primaryBlue,
         secondary: softGreen,
@@ -30,6 +39,25 @@ class AppTheme {
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
         ),
       ),
+    );
+  }
+
+  // Dark Theme
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSeed(
+        brightness: Brightness.dark,
+        seedColor: primaryBlue,
+        primary: primaryBlueDark,
+        surface: darkSurface,
+        background: darkBackground,
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
+        scaffoldBackgroundColor: darkBackground
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:child_and_student_care_and_tracking_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  MaterialApp(
+      title: 'Student Care Track System',
+      debugShowCheckedModeBanner: false,
+      //our themes applied here
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+
+      //the ff setting lets the system choose(or we can force one)
+      themeMode: ThemeMode.system,
+      home: Center(
+        child: Text('Theme is now active'),
+      ),
+    );
   }
 }
